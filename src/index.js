a3882abe2bccc51b5028f622c2d5f83aeebd2140
kathmontalvo <kathmontalvo123@gmail.com>
2019-06-10 00:56:27 -0500

    extrayendo links con marked

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
+const marked = require('marked')
 
 const converToAbs = (relRoute) => {
   return path.resolve(relRoute);
@@ -31,26 +32,7 @@ const isFile = (route) => {
   return stats.isFile()
 }
 
-// const filePath = (route) => {
-//   // let arrFiles = []
 
-//   if (isFile(route)) {
-//     // arrFiles.push(route);
-//     console.log('arrFiles')
-//   }
-// if(isDirectory(route)) {
-//   fs.readdir(route, (err, files) => {
-//     if (err) throw err;
-
-//     files.forEach((f) => {
-//       console.log(f)
-//       arrFiles.concat(filePath(path.join(route, f)))
-//     })
-//   })
-// }
-//   // console.log(arrFiles)
-//   // return arrFiles
-// }
 
 let arrFiles = []
 export const filePath = (route) => {
@@ -59,37 +41,50 @@ export const filePath = (route) => {
   }
   else if (!isFile(route)) {
     fs.readdirSync(route).forEach((f) => {
-     filePath(path.join(route, f))
+      filePath(path.join(route, f))
     })
   }
   return arrFiles
 }
 
-// const isRouteFile = (route) => {
-//   let arrFiles = []
-//   return fs.stat(route, (err, stats) => {
-//     if (err) throw err;
-//     if (stats.isFile() && path.extname(route) === '.md') {
-//       arrFiles.push(route);
-//       console.log(arrFiles)
-//     } else if (stats.isDirectory()) {
-//       fs.readdir(route, (err, files) => {
-//         if (err) throw err;
-//         files.forEach((f) => {
-//           arrFiles = arrFiles.concat(isRouteFile(path.join(route, f)))
-//         })
-//       })
-//     }
-//     return arrFiles
-//   })
-// }
 
 // const route = process.argv[1];
 const route = 'C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files';
 const file = 'C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files\\nivel1\\markdown.md'
 
+const fileContent = (route) => {
+  const arrContent = []
+  filePath(route).forEach(file => {
+    arrContent.push({
+      content: fs.readFileSync(file).toString(),
+      file: file
+    })
+  })
+  return arrContent
+}
 
-// filePath(route).forEach( file => {
-//    console.log([fs.readFileSync(file).toString()])
-// })
 
+fileContent(route).forEach(res => {
+  // const tokens = marked.lexer(cont);
+  // const html = marked.parser(tokens);
+  // // console.log(html);
+  // console.log(tokens.links);
+
+  // https://github.com/tcort/markdown-link-extractor/blob/master/index.js
+
+  const links = [];
+
+  const renderer = new marked.Renderer();
+
+  renderer.link =  (href, title, text) => {
+    links.push({
+      href: href,
+      text: text,
+      file: res.file
+    });
+  };
+
+  marked(res.content, { renderer: renderer });
+  console.log(links)
+  return links;
+})
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,4 @@
-import { isPathAbsolute, convertToAbs, pathIsFile, checkExt, readMyDir, filePath } from '../src/index.js'
+import { isPathAbsolute, filePath } from '../src/index.js'
 
 describe('función isPathAbsolute', () => {
   it('deberia retornar true si la ruta es absoluta', () => {
@@ -11,7 +11,7 @@ describe('función isPathAbsolute', () => {
 });
 
 describe('funcion filePath', () => {
-  it.only('deberia retornar un array con las rutas abs .md', () => {
+  it('deberia retornar un array con las rutas abs .md', () => {
     expect(filePath('C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files')).toEqual([
       'C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files\\nivel1\\markdown.md', 'C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files\\nivel1\\nivel2\\new\\file.md', 'C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files\\nivel1\\nivel2\\prueba.md', 'C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files\\nivel1\\nivel2\\readme.md', 'C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files\\nivel1-archivos\\hola.md', 'C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files\\nivel1-archivos\\readme.md', 'C:\\Users\\Kathlen\\Google Drive\\Programación\\Laboratoria\\bootcamp\\Pruebas\\md-files\\readme.md'
     ])
